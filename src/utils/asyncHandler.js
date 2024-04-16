cc93d8b5fbbb6de40c949025efe144fdc6a46bc7
Hritik <pypi.pioneer@gmail.com>
2024-04-16 16:59:10 +0530

    added utils to standardize response and error

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,11 @@
+// to handle async operations
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .reject((err) => next(err));
+    }
+}
+
+
+export { asyncHandler };
