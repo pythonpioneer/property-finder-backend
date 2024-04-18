5ed35cd22060294d388283c290d1278d984c3678
Hritik <pypi.pioneer@gmail.com>
2024-04-18 17:56:41 +0530

    now, we can upload more images to properties db

--- src/routes/property.js
+++ src/routes/property.js
@@ -1,6 +1,6 @@
 // importing requirements
 const router = require('express').Router();
-const { addProperty, fetchOneProperty } = require('../controllers/property');
+const { addProperty, fetchOneProperty, addMoreImage } = require('../controllers/property');
 const { fetchUser } = require('../middlewares/auth/authMiddleware');
 const { uploadImage } = require('../middlewares/uploads/multer.middleware');
 const { validatePropertyFields, validateMongoDbObjectId } = require('../middlewares/validation/validationField');
@@ -10,8 +10,11 @@ const validateValidationResult = require('../middlewares/validation/validationMi
 // Route 1: To add a new property: '/api/v1/property/' [using POST] (login required)
 router.post('/', uploadImage('image'), validatePropertyFields, validateValidationResult, fetchUser, addProperty);
 
-// Route 1: To add fetch any property: '/api/v1/property/:propertyId' [using POST] (login not required)
+// Route 2: To add fetch any property: '/api/v1/property/:propertyId' [using POST] (login not required)
 router.get('/:propertyId', validateMongoDbObjectId, validateValidationResult, fetchOneProperty);
+
+// Route 3: To add more images to property: '/api/v1/property/:propertyId/images' [using POST] (login required)
+router.patch('/:propertyId/images', uploadImage('image'), validateMongoDbObjectId, validateValidationResult, fetchUser, addMoreImage);
 
 
 // export all the routes
